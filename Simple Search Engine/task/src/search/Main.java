d3114225fb2850fe5943b5f6fca2762cad36f232
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2022-03-28 12:09:27 +0200

    Expand the search

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -1,22 +1,42 @@
 package search;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final ArrayList<String> userData = new ArrayList<>();
+
 
     public static void main(String[] args) {
-        ArrayList<String> input = Arrays.stream(scanner.nextLine().split(" "))
-                .collect(Collectors.toCollection(ArrayList::new));
-        String word = scanner.nextLine();
+        System.out.println("Enter the number of people:");
+        int dataNum = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < dataNum; i++) {
+            userData.add(scanner.nextLine());
+        }
+        System.out.println();
 
-        if (input.contains(word)) {
-            System.out.println(input.indexOf(word) + 1);
-        } else {
-            System.out.println("Not found");
+        System.out.println("Enter the number of search queries:");
+        int queriesNum = Integer.parseInt(scanner.nextLine());
+        System.out.println();
+
+        for (int i = 0; i < queriesNum; i++) {
+            System.out.println("Enter data to search people:");
+            String query = scanner.nextLine();
+
+            Pattern pattern = Pattern.compile(query, Pattern.CASE_INSENSITIVE);
+            if (userData.stream().noneMatch(d -> pattern.matcher(d).find())) {
+                System.out.println("No matching people found.");
+                System.out.println();
+            } else {
+                System.out.println();
+                System.out.println("Found people:");
+                userData.stream().filter(d -> pattern.matcher(d).find()).forEach(System.out::println);
+            }
+            System.out.println();
         }
     }
 }
